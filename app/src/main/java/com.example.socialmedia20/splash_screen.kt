5e0712957a7b61dc5d364e341e89f49a73f2aefc
Chandan Singha <chandan094.hitit2020@gmail.com>
2023-01-02 03:57:40 +0530

    splash screen added

---
+++ app/src/main/java/com.example.socialmedia20/splash_screen.kt
@@ -0,0 +1,56 @@
+package com.example.socialmedia20
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.socialmedia20.databinding.SplashScreenBinding
+
+class splash_screen : AppCompatActivity() {
+
+    lateinit var binding:SplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val motionLayout: MotionLayout= binding.motionLayout
+        motionLayout.addTransitionListener(object :MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@splash_screen,MainActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+    }
+}
+
